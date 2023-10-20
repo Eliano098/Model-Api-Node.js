f33eed49ac8e09fd6663483b9859ca47e4ba9c74
Eliano Cordeiro <elianocordeiro18@gmail.com>
2023-10-19 22:23:13 -0300

    refactor: connection database sequelize

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,6 +1,22 @@
 const Sequelize = require('sequelize')
-const configDatabase = require('./database')
+require('dotenv').config();
 
-const sequelize = new Sequelize(configDatabase);
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'postgres',
+/*        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            },
+        }
+*/
+    }
+);
 
 module.exports = sequelize;
